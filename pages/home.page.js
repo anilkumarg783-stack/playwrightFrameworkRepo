240b19acd69c605ea1cb7f2726472de102a6de29
anilkumarg783-stack <anilkumarg783@gmail.com>
2026-06-23 11:48:23 +0530

    added dashboard menu in homepage

--- pages/home.page.js
+++ pages/home.page.js
@@ -5,6 +5,7 @@ class home{
         this.homeText=page.locator('a.hdrLink')
         this.moreMenu=page.locator('//a[text()="More"]')
         this.moreVendorSubMenu=page.locator('//a[@href="index.php?module=Vendors&action=index"]')
+        this.dashboardMenu=page.locator('//a[@href="index.php?module=Dashboard&action=index"]')
     }
 }
 export default home 
